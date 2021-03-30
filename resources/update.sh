97a9e6e711124c84402be31aa82a5163999e2c66
aemyers <32523187+aemyers@users.noreply.github.com>
2021-03-29 22:42:19 -0700

    automatically update at service start

--- resources/update.sh
+++ resources/update.sh
@@ -1,1 +1,19 @@
-/usr/games/steamcmd +runscript {RESOURCES}/update.steam
+#!/bin/bash
+
+installed=$(grep -Po '(?<="buildid"\t\t").*(?=")' '{INSTALL}/steamapps/appmanifest_896660.acf')
+info=$(/usr/games/steamcmd +login anonymous +app_info_update 1 +app_info_print 896660 +quit)
+available=$(echo "$info" \
+	| sed -n '/"branches"/,/^}/p' \
+	| sed -n '/"public"/,/}/p' \
+	| grep -Po '(?<="buildid"\t\t").*(?=")' \
+	)
+
+echo "installed: ${installed}"
+echo "available: ${available}"
+if [[ "${installed}" == "${available}" ]]; then
+	echo 'already installed'
+	exit
+fi
+
+echo 'updating...'
+/usr/games/steamcmd +runscript '{RESOURCES}/update.steam'
