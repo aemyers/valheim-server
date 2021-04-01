d4447b72a443cbbc2acc4eba2c10bf722995359b
aemyers <32523187+aemyers@users.noreply.github.com>
2021-04-01 07:26:11 -0700

    inline steamcmd script for update

--- resources/update.sh
+++ resources/update.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-installed=$(grep -Po '(?<="buildid"\t\t").*(?=")' '{INSTALL}/steamapps/appmanifest_896660.acf')
-info=$(/usr/games/steamcmd +login anonymous +app_info_update 1 +app_info_print 896660 +quit)
+INSTALL='{INSTALL}'
+APPID='896660' # Valheim Dedicated Server https://steamdb.info/app/896660/depots/?branch=public
+STEAMCMD='/usr/games/steamcmd'
+
+installed=$(grep -Po '(?<="buildid"\t\t").*(?=")' "$INSTALL/steamapps/appmanifest_${APPID}.acf")
+
+info=$("$STEAMCMD" \
+	+login anonymous \
+	+app_info_update 1 \
+	+app_info_print "$APPID" \
+	+quit)
 available=$(echo "$info" \
 	| sed -n '/"branches"/,/^}/p' \
 	| sed -n '/"public"/,/}/p' \
 	| grep -Po '(?<="buildid"\t\t").*(?=")' \
 	)
 
-echo "installed: ${installed}"
-echo "available: ${available}"
-if [[ "${installed}" == "${available}" ]]; then
-	echo 'already installed'
+echo "installed: $installed"
+echo "available: $available"
+
+if [[ "$installed" == "$available" ]]; then
+	echo 'no update available'
 	exit
 fi
 
-echo 'updating...'
-/usr/games/steamcmd +runscript '{RESOURCES}/update.steam'
+echo 'update found; installing...'
+"$STEAMCMD" \
+	+login anonymous \
+	+force_install_dir "$INSTALL" \
+	+app_update "$APPID" validate
+	+quit
