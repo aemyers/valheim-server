1f003aa968b40287fad670db38d59049a6fea65d
Allen Myers <aemyers4@gmail.com>
2025-07-21 14:29:02 -0500

    refactor to posix compliant

--- resources/monitor.sh
+++ resources/monitor.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
 set -o pipefail
@@ -7,21 +7,19 @@ readonly UTILS=utils
 . "${UTILS}/properties.sh"
 . "${UTILS}/discord.sh"
 
-declare -ri DEATH=0
-declare -ri JOIN=20
+readonly PROPERTIES='monitor.properties'
 
-. util/json.sh
-. util/discord.sh
-. util/properties.sh
+readonly DISCORD_TOKEN=$(properties_read "${PROPERTIES}" "token")
+readonly CHANNEL_NOTIFY=$(properties_read "${PROPERTIES}" "channel.notify")
+readonly CHANNEL_STATUS=$(properties_read "${PROPERTIES}" "channel.status")
 
-declare -r DISCORD_TOKEN=$(properties_read "${PROPERTIES}" "token")
-declare -r CHANNEL_NOTIFY=$(properties_read "${PROPERTIES}" "channel.notify")
-declare -r CHANNEL_STATUS=$(properties_read "${PROPERTIES}" "channel.status")
+readonly DEATH=0
+readonly JOIN=20
 
-declare -i COUNT=0
+COUNT=0
 
 status() {
-	local -ri count=$1
+	count=$1
 
 	COUNT=$count
 	discord_topic "${CHANNEL_NOTIFY}" "${COUNT} connected"
@@ -29,28 +27,28 @@ status() {
 }
 
 parse_content() {
-	local -r line="${1}"
+	line="${1}"
 	# 'Apr 03 02:00:26 ovh-va-valheim valheim_server.x86_64[92662]: 04/03/2024 02:00:26: Random event set:army_theelder'
 
-	local -r raw=$(cut --delimiter=':' --fields=7- <<< "${line}")
+	raw=$(cut --delimiter=':' --fields=7- <<< "${line}")
 	# ' Random event set:army_theelder'
 
-	local -r result="${raw:1}"
+	result="${raw:1}"
 	# 'Random event set:army_theelder'
 
 	echo "${result}"
 }
 
 parse() {
-	local -r content=$(parse_content "${1}")
+	content=$(parse_content "${1}")
 
 	# 'Got character ZDOID from Name With Spaces  : 1234567890:1' # '<id>:<seconds connected>', '0:0' is death
 	if grep --quiet --regexp='^Got character ZDOID from' <<< "${content}"; then
-		local -r data=$(cut --delimiter=' ' --fields=5- <<< "${content}") # 'Name With Spaces  : 1234567890:1'
-		local -r duration=$(cut --delimiter=':' --fields=3 <<< "${data}") # '1'
+		data=$(cut --delimiter=' ' --fields=5- <<< "${content}") # 'Name With Spaces  : 1234567890:1'
+		duration=$(cut --delimiter=':' --fields=3 <<< "${data}") # '1'
 		if [ $duration -ne $DEATH ] && [ $duration -lt $JOIN ]; then
-			local -r character=$(cut --delimiter=':' --fields=1 <<< "${data}")  # 'Name With Spaces '
-			local -r trimmed=$(sed -e 's/[[:space:]]*$//' <<< "${character}")   # 'Name With Spaces'
+			character=$(cut --delimiter=':' --fields=1 <<< "${data}")  # 'Name With Spaces '
+			trimmed=$(sed -e 's/[[:space:]]*$//' <<< "${character}")   # 'Name With Spaces'
 			discord_message "${CHANNEL_NOTIFY}" "${trimmed}: I HAVE ARRIVED!"
 			status $(( COUNT + 1 ))
 		fi
@@ -61,8 +59,8 @@ parse() {
 
 	# "Player connection lost server "valheim.aemyers.com" that has join code 123456, now 1 player(s)"
 	elif grep --quiet --regexp='^Player connection lost' <<< "${content}"; then
-		local -ri after=$(cut --delimiter='"' --fields=3 <<< "${content}")
-		local -ri count=$(cut --delimiter=' ' --fields=8 <<< "${after}")
+		after=$(cut --delimiter='"' --fields=3 <<< "${content}")
+		count=$(cut --delimiter=' ' --fields=8 <<< "${after}")
 		status $count
 
 	# "Apr 08 21:36:21 ovh-va-valheim systemd[1]: Stopping valheim..."
@@ -73,27 +71,27 @@ parse() {
 
 	# "Valheim version: l-0.217.38 (network version 20)"
 	elif grep --quiet --regexp='^Valheim version' <<< "${content}"; then
-		local -r data=$(cut --delimiter=' ' --fields=3 <<< "${content}")
-		local -r version=$(cut --delimiter='-' --fields=2 <<< "${data}")
+		data=$(cut --delimiter=' ' --fields=3 <<< "${content}")
+		version=$(cut --delimiter='-' --fields=2 <<< "${data}")
 		discord_message "${CHANNEL_NOTIFY}" "started version ${version}"
 		status 0
 
  	# " Connections 2 ZDOS:222503  sent:57 recv:216"
 	elif grep --quiet --regexp='^ Connections' <<< "${content}"; then
-		local -ri count=$(cut --delimiter=' ' --fields=3 <<< "${content}")
+		count=$(cut --delimiter=' ' --fields=3 <<< "${content}")
 		status $count
 
 	# "Random event set:army_theelder"
 	elif grep --quiet --regexp='^Random event set' <<< "${content}"; then
-		local -r event=$(cut --delimiter=':' --fields=2 <<< "${content}")
-		local -r description=$(properties_read "${PROPERTIES}" "event.${event}")
+		event=$(cut --delimiter=':' --fields=2 <<< "${content}")
+		description=$(properties_read "${PROPERTIES}" "event.${event}")
 		discord_message "${CHANNEL_NOTIFY}" "random event started: \"${description}\" (${event})"
 
 	fi
 }
 
 main() {
-	local -r service=$(properties_read "${PROPERTIES}" "service")
+	service=$(properties_read "${PROPERTIES}" "service")
 
 	while true; do # until killed
 
